c09dd31cba3851f679dbb9197668abd01f77cc0c
Joe Martin <joe@desertflood.com>
2024-08-10 00:15:40 -0700

    feat: auto activate direnv
    Signed-off-by: Joe Martin <joe@desertflood.com>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import shutil
 import subprocess
 import sys
 
@@ -13,18 +14,18 @@ def remove_file(filepath):
         pass
 
 
-def execute(*args, supress_exception = False, cwd=None):
+def execute(*args, supress_exception=False, cwd=None):
     cur_dir = os.getcwd()
 
     try:
         if cwd:
             os.chdir(cwd)
 
-        proc = subprocess.Popen(args, stdout = subprocess.PIPE, stderr= subprocess.PIPE)
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         out, err = proc.communicate()
-        out = out.decode('utf-8')
-        err = err.decode('utf-8')
+        out = out.decode("utf-8")
+        err = err.decode("utf-8")
         if err and not supress_exception:
             raise Exception(err)
         else:
@@ -36,7 +37,6 @@ def execute(*args, supress_exception = False, cwd=None):
 def init_git():
     # workaround for issue #1
     if not os.path.exists(os.path.join(PROJECT_DIRECTORY, ".git")):
-        execute("git", "config", "--global", "init.defaultBranch", "main", cwd = PROJECT_DIRECTORY)
         execute("git", "init", cwd=PROJECT_DIRECTORY)
 
 
@@ -45,23 +45,47 @@ def install_pre_commit_hooks():
     execute(sys.executable, "-m", "pre_commit", "install")
 
 
-if __name__ == '__main__':
+def activate_direnv():
+    if not os.path.exists(os.path.join(PROJECT_DIRECTORY, ".envrc")):
+        shutil.copy2(os.path.join(PROJECT_DIRECTORY, "envrc"), os.path.join(PROJECT_DIRECTORY, ".envrc"))
+        execute("direnv", "allow", PROJECT_DIRECTORY)
+        if os.path.exists(os.path.join(PROJECT_DIRECTORY, ".git")):
+            # Nix can't setup the devenv unless the files have been Added to git
+            execute("git", "add", "devenv.nix", "flake.nix", cwd=PROJECT_DIRECTORY)
 
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.pkg_name }}', 'cli.py')
+if __name__ == "__main__":
+    if "no" in "{{ cookiecutter.command_line_interface|lower }}":
+        cli_file = os.path.join("{{ cookiecutter.pkg_name }}", "cli.py")
         remove_file(cli_file)
 
-    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
-        remove_file('LICENSE')
+    if "Not open source" == "{{ cookiecutter.open_source_license }}":
+        remove_file("LICENSE")
 
     try:
         init_git()
     except Exception as e:
         print(e)
 
-    if '{{ cookiecutter.install_precommit_hooks }}' == 'y':
+    if "{{ cookiecutter.install_precommit_hooks }}" == "y":
         try:
             install_pre_commit_hooks()
         except Exception as e:
             print(str(e))
-            print("Failed to install pre-commit hooks. Please run `pre-commit install` by your self. For more on pre-commit, please refer to https://pre-commit.com")
+            print(
+                "Failed to install pre-commit hooks. Please run `pre-commit install` by your self. For more on pre-commit, please refer to https://pre-commit.com"
+            )
+    else:
+        remove_file(".pre-commit-config.yaml")
+
+
+    if "{{ cookiecutter.activate_direnv }}" == "y":
+        try:
+            activate_direnv()
+            print(
+                "If necessary, run `nix-direnv-reload` to setup the cached devenv."
+            )
+        except Exception as e:
+            print(str(e))
+            print(
+                "Failed to activate direnv integration. Please copy `envrc` to `.envrc` yourself, then run `nix-direnv-reload`."
+            )
